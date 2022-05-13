7bc80e68298f55190946da8fe4a2d33e60a51703
Cervon Wong <tcrammy@gmail.com>
2022-05-13 16:56:45 +0800

    Remove hash from URL

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'screens/welcome_screen.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const MyApp());
 }
 
